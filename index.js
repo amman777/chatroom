3447dc2f1a2e2c4ed6430461524c72530aa3f405
Amman <100275295+amman777@users.noreply.github.com>
2023-04-29 21:49:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ window.onload = function () {
             var join_input = document.createElement('input')
             join_input.setAttribute('id', 'join_input')
             join_input.setAttribute('maxlength', 15)
-            join_input.placeholder = 'Hello its a chat room'
+            join_input.placeholder = 'Hello! Enter Your Name to Join'
             // Every time we type into the join_input
             join_input.onkeyup = function () {
                 // If the input we have is longer that 0 letters
